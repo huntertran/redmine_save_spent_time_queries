61018d5c6f927ede2b912af04795d9bfe6a1bc47
Stefan Moises <moises@shoptimax.de>
2016-06-20 11:02:52 +0200

    Basic Redmine 3.x compatibility

--- assets/javascripts/save_spent_time_queries.js
+++ assets/javascripts/save_spent_time_queries.js
@@ -1,23 +1,17 @@
-
 // Create top menu
 var spent_time_queries_menu = $("#top-menu ul li a.spent-time-query").parent();	
 var spent_time_ul = "<ul>";
-
 $("#spent_time_queries a").each(function() {
 	spent_time_ul += "<li>" + $(this).clone().wrap('<div>').parent().html() + "</li>";		
 });
-
 spent_time_ul += "</ul>";
-
 if($('#top-menu ul').dropit && spent_time_ul != '<ul></ul>'){
 	spent_time_queries_menu.append(spent_time_ul);		
 	$("#top-menu ul li a.spent-time-query").html($("#top-menu ul li a.spent-time-query").html() + " ▽");	
-	$('#top-menu ul').dropit({ action: 'hover' });		
+	$('#top-menu ul').dropit({ action: 'mouseenter' });		
 }
-
 $("#top-menu").css("visibility", "visible");
 $("#main").css("visibility", "visible");
-
 // Decorate
 var href = decodeURIComponent(location.href);
 if(href.indexOf("&v[query]=") != -1 && href.indexOf("/time_entries") != -1) {	
